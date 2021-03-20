d34f1619ba651810b89a5f0485648b5c5382c5ad
MVesliga <martxyx@gmail.com>
2021-03-20 10:41:52 +0100

    added initial project structure

---
+++ src/main/java/hr/tvz/loveme/LovemeApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.loveme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/tvz/loveme/LovemeApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.loveme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
