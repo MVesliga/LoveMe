145c34c192066170e4aeb20b22a50dbd1dbc90b7
jelena1912 <jberkovic93@gmail.com>
2021-04-12 22:35:00 +0200

    Selenium test import fix

--- src/test/java/hr/tvz/loveme/selenium/LoginTests.java
+++ src/test/java/hr/tvz/loveme/selenium/LoginTests.java
@@ -1,14 +1,11 @@
 package hr.tvz.loveme.selenium;
 
 import lombok.SneakyThrows;
-import org.junit.Before;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
