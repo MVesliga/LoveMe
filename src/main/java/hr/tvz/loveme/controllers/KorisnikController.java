c7955e68c3d421334e9a42a63590080ca84760e2
MVesliga <martxyx@gmail.com>
2021-04-17 14:44:47 +0200

    [task/h2] dodan prikaz username-a ulogiranog korisnika

--- src/main/java/hr/tvz/loveme/controllers/KorisnikController.java
+++ src/main/java/hr/tvz/loveme/controllers/KorisnikController.java
@@ -2,11 +2,14 @@ package hr.tvz.loveme.controllers;
 
 import hr.tvz.loveme.domain.Korisnik;
 import hr.tvz.loveme.repository.KorisnikRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -20,10 +23,10 @@ public class KorisnikController {
     }
 
     @GetMapping("/profil")
-    public String getProfil(Model model) {
+    public String getProfil(Model model, Principal principal) {
         //TODO makmnuti kasnije
         List<Korisnik> listaKorisnika = korisnikRepository.findAll();
-        Korisnik korisnik = korisnikRepository.findById(1).get();
+        System.out.println(principal.getName());
         System.out.println("tu sam!");
 
         return "profil";
