ac8671705e6938378e5d456e6c386849cc4e316b
MVesliga <martxyx@gmail.com>
2021-04-18 17:15:10 +0200

    [task/admin] dodana mogucnost brisanja korisnika sa adminom

--- src/main/java/hr/tvz/loveme/controllers/AdminController.java
+++ src/main/java/hr/tvz/loveme/controllers/AdminController.java
@@ -1,10 +1,11 @@
 package hr.tvz.loveme.controllers;
 
+import hr.tvz.loveme.domain.Korisnik;
 import hr.tvz.loveme.facade.KorisnikFacade;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/love-me")
@@ -21,5 +22,16 @@ public class AdminController {
         model.addAttribute("listaKorisnika", korisnikFacade.getKorisnikRepository().findAll());
 
         return "korisnici";
+    }
+
+    @GetMapping("/delete/korisnik")
+    public String deleteKorisnik(@RequestParam("korisnikId") Integer korisnikId,
+                                 RedirectAttributes redirectAttributes) {
+        Korisnik korisnik = korisnikFacade.getKorisnikRepository().findById(korisnikId).get();
+        korisnikFacade.getKorisnikUlogaRepository().deleteByKorisnickoIme(korisnik.getKorisnickoIme());
+        korisnikFacade.getKorisnikRepository().deleteById(korisnikId);
+
+        redirectAttributes.addFlashAttribute("deleteKorisnikSuccess", true);
+        return "redirect:/love-me/korisnici";
     }
 }
--- src/main/java/hr/tvz/loveme/repository/KorisnikUlogaRepository.java
+++ src/main/java/hr/tvz/loveme/repository/KorisnikUlogaRepository.java
@@ -3,5 +3,9 @@ package hr.tvz.loveme.repository;
 import hr.tvz.loveme.domain.KorisnikUloga;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
 public interface KorisnikUlogaRepository extends JpaRepository<KorisnikUloga, Integer> {
+    @Transactional
+    void deleteByKorisnickoIme(String korisnickoIme);
 }
