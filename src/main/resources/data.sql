8a9b8f54eab45bc0804a022b974a61143d55236d
Deborah <deborahgomercic@gmail.com>
2021-04-19 22:55:42 +0200

    ljubimci gotovi osim što edit ne radi

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,8 @@ VALUES ('Pero', 'Peric', '1997-07-29', 'pero@mail.com', 'pero', '$2y$12$tvFvkRFN
 INSERT INTO korisnik_uloga (korisnicko_ime, uloga) VALUES ('pero', 'ROLE_USER');
 
 INSERT INTO ljubimac (ime, korisnik_id, korisnicko_ime, vrsta, dob, veterinar, cijepljen, obuka, hrana, igracka)
-VALUES ('Perin pas', 1, 'pero', 'pas', 2, TO_DATE('17/12/2015', 'DD/MM/YYYY'), 'da', 'ne', 'hrana', 'igracka'),
-('Perina macka', 1, 'pero', 'macka', 7, TO_DATE('17/12/2016', 'DD/MM/YYYY'), 'ne', 'ne', 'mjau', 'mjau');
+VALUES ('Perin pas', 1, 'pero', 'Africki tvor', 2, TO_DATE('17/12/2015', 'DD/MM/YYYY'), 'da', 'ne', 'hrana', 'igracka'),
+('Perina macka', 1, 'pero', 'Macka', 7, TO_DATE('17/12/2016', 'DD/MM/YYYY'), 'ne', 'ne', 'mjau', 'mjau'),
+('Perina macka', 1, 'pero', 'Patuljasta svinja', 7, TO_DATE('17/12/2016', 'DD/MM/YYYY'), 'ne', 'ne', 'mjau', 'mjau'),
+('Perina macka', 1, 'pero', 'Kunic', 7, TO_DATE('17/12/2016', 'DD/MM/YYYY'), 'ne', 'ne', 'mjau', 'mjau'),
+('Perina macka', 1, 'pero', 'Cincila', 7, TO_DATE('17/12/2016', 'DD/MM/YYYY'), 'ne', 'ne', 'mjau', 'mjau');
